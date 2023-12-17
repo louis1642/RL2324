23bc4bd2cbe538294234fc3614811b5ac932b0ea
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-12-17 17:44:09 +0100

    .gitignore is now working

--- src/hw1/arm_controller/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luigi/catkin_ws/src/arm_controller
-# Build directory: /home/luigi/catkin_ws/src/arm_controller/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
