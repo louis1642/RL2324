ebfa375787e57a020777b135b7064841230cb35a
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-12-17 17:40:25 +0100

    bash to launch clion for each pkg

---
+++ hw1Clion.bash
@@ -0,0 +1,3 @@
+catkin_make
+source devel/setup.bash
+clion src/hw1/arm_controller/CMakeLists.txt src/hw1/arm_gazebo/CMakeLists.txt src/hw1/arm_control/CMakeLists.txt src/hw1/arm_description/CMakeLists.txt
