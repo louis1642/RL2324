193746f1d7327117d89ddeec7eac99eb1155e761
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-12-17 20:14:46 +0100

    comments

--- hw1Clion.bash
+++ hw1Clion.bash
@@ -1,3 +1,4 @@
 catkin_make
 source devel/setup.bash
-clion src/hw1/arm_controller/CMakeLists.txt src/hw1/arm_gazebo/CMakeLists.txt src/hw1/arm_control/CMakeLists.txt src/hw1/arm_description/CMakeLists.txt
+#clion src/hw1/arm_controller/CMakeLists.txt src/hw1/arm_gazebo/CMakeLists.txt src/hw1/arm_control/CMakeLists.txt src/hw1/arm_description/CMakeLists.txt
+clion src/CMakeLists.txt
